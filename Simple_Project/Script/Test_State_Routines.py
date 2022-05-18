a59b624bb8e02748a3793e63bd626c80920896ca
Kevin-M-Johnson <101289324+Kevin-M-Johnson@users.noreply.github.com>
2022-05-18 16:39:03 -0400

    Initial Commit

---
+++ Simple_Project/Script/Test_State_Routines.py
@@ -0,0 +1,18 @@
+﻿#******************
+# Test_State_Routines - The routines below are simplle log messages that exercises the varios stated outcomes of tests (Pass/Fail/Warning)
+# Created 20220427 - KMJ
+#
+#******************
+
+def Failing_Test():
+    #Posts an error to the test log.
+    Log.Error("This is simply a test to demonstrate error execution", "", pmNormal, Project.Variables.LogAtrribFailed)
+
+#@Seed
+def Passing_Test():
+    #Posts an information message to the test log.
+    Log.Message("This is simply a test to demostrate passing execution", "", pmNormal, Project.Variables.LogAtrribInformation)
+
+def Warning_Test():
+    #Posts a warning to the test log.
+    Log.Warning("This is simply a test to demostrate warning execution", "", pmNormal, Project.Variables.LogAtrribWarning)
